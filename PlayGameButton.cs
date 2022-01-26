d73ee99d6b7e0b02c88f5149a5edf0b6e29033a3
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2022-01-26 15:34:58 +0300

    create a seperate scene for main menu

---
+++ PlayGameButton.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class PlayGameButton : Button
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GetTree().ChangeScene("res://MainScene.tscn");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}

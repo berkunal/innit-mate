78cb117f13696e485c84c7e67a4306641c68e905
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2022-01-26 16:01:49 +0300

    make "start game" and "exit game" buttons function

--- ExitGameButton.cs
+++ ExitGameButton.cs
@@ -3,19 +3,26 @@ using System;
 
 public class ExitGameButton : Button
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GetTree().Quit();
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
+	private void _on_ExitGameButton_pressed()
+	{
+		GetTree().Quit();
+	}
 }
+
+
+
--- PlayGameButton.cs
+++ PlayGameButton.cs
@@ -3,19 +3,24 @@ using System;
 
 public class PlayGameButton : Button
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GetTree().ChangeScene("res://MainScene.tscn");
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
+	private void _on_PlayGameButton_pressed()
+	{
+		GetTree().ChangeScene("res://MainScene.tscn");
+	}
 }
+
